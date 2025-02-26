d7db01bc04e4db25de7aa6b1c7a50c0f552d6ef9
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-02-26 00:59:09 -0500

    final push for the night

--- order/src/main/java/org/order/inventory/InventoryConfig.java
+++ order/src/main/java/org/order/inventory/InventoryConfig.java
@@ -1,19 +1,19 @@
 package org.order.inventory;
 
-import io.grpc.Grpc;
-import io.grpc.InsecureChannelCredentials;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.grpc.client.GrpcChannelFactory;
 import proto.client.InventoryServiceGrpc;
 
 @Configuration
 class InventoryConfig {
 
     @Bean
-    InventoryServiceGrpc.InventoryServiceBlockingStub stub(final Environment env) {
-        final int port = env.getProperty("grpc.inventory.port", Integer.class, 2022);
-        final var channel = Grpc.newChannelBuilder("localhost:" + port, InsecureChannelCredentials.create()).build();
+    InventoryServiceGrpc.InventoryServiceBlockingStub stub(final Environment env, final GrpcChannelFactory channels) {
+        final String port = "0.0.0.0:" + env.getProperty("grpc.inventory.port", Integer.class, 9090);
+//        final var channel = Grpc.newChannelBuilder("localhost:" + port, InsecureChannelCredentials.create()).build();
+        final var channel = channels.createChannel(port);
         return InventoryServiceGrpc.newBlockingStub(channel);
     }
 
